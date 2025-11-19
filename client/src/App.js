f4f56e70a9fe50d99c52b648717f305f73ddd7a5
Krish Arul Meiyappan <krish.arulmeiyappan@gmail.com>
2025-11-18 17:52:38 -0800

    Create App.js file for ConnectSphere

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ConnectSphere</h1>
+      <p>Your social media app scaffolding is ready!</p>
+    </div>
+  );
+}
+
+export default App;
